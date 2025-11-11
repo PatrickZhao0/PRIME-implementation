9ed455d66bb8b7c207d4f1d2ba8d759c69f59e3f
Bailong Zhao <bz2099@nyu.edu>
2025-11-12 03:02:15 +0800

    s

--- datasets/base.py
+++ datasets/base.py
@@ -117,7 +117,7 @@ class AbstractDataset(metaclass=ABCMeta):
     def _get_preprocessed_folder_path(self):
         preprocessed_root = self._get_preprocessed_root_path()
         folder_name = '{}_min_rating{}-min_uc{}-min_sc{}-{}' \
-            .format(self.code(), self.min_rating, self.min_uc, self.min_sc, self.split)
+            .format(self.code(), self.min_rating, self.min_uc, self.min_sc)
         return preprocessed_root.joinpath(folder_name)
 
     def _get_preprocessed_dataset_path(self):
