b8bbe2052508a9af2bdd3ca70d2392c6f107f51e
Bailong Zhao <bz2099@nyu.edu>
2025-11-12 02:48:52 +0800

    s

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,19 +1,7 @@
-from .ml_1m import ML1MDataset
-from .beauty import BeautyDataset
-from .video import VideoDataset
-from .sports import SportsDataset
-from .steam import SteamDataset
-from .xlong import XLongDataset
 from .toys import ToysDataset
 
 
 DATASETS = {
-    ML1MDataset.code(): ML1MDataset,
-    BeautyDataset.code(): BeautyDataset,
-    VideoDataset.code(): VideoDataset,
-    SportsDataset.code(): SportsDataset,
-    SteamDataset.code(): SteamDataset,
-    XLongDataset.code(): XLongDataset,
     ToysDataset.code(): ToysDataset,
 }
 
