2bb8cf530ce1cc908750a5852007973e310640bf
Bailong Zhao <bz2099@nyu.edu>
2025-11-11 21:49:11 +0800

    s

--- model/CLIP.py
+++ model/CLIP.py
@@ -1,16 +1,29 @@
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
+from pathlib import Path
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+model.eval()
 
-image_paths = ['data/toys/images/1.jpg', 'data/toys/images/2.jpg', 'data/toys/images/3.jpg']
+choice = input("Enter dataset name: ")
+photo_dir = Path("photos") / choice
+image_paths = sorted(photo_dir.glob("*"))
 
-images = [Image.open(p).convert("RGB") for p in image_paths]
-inputs = processor(images=images, return_tensors="pt").to(device)
+all_embeds = []
 with torch.no_grad():
-    embeds = model.get_image_features(**inputs)
-    embeds = embeds / embeds.norm(dim=-1, keepdim=True)
-    torch.save(embeds, "image_embeds.pt")
+    batch_size = 32
+    for i in range(0, len(image_paths), batch_size):
+        batch_paths = image_paths[i : i + batch_size]
+        images = [Image.open(p).convert("RGB") for p in batch_paths]
+        inputs = processor(images=images, return_tensors="pt").to(device)
+        embeds = model.get_image_features(**inputs)
+        embeds = embeds / embeds.norm(dim=-1, keepdim=True)
+        all_embeds.append(embeds.cpu())
+        print(f"Processed {i + len(batch_paths)}/{len(image_paths)}")
+
+embeds = torch.cat(all_embeds, dim=0)
+torch.save(embeds, Path("photos/embeddings") / f"{choice}_image_embeds.pt")
+print(f"\n Saved {len(embeds)} embeddings to {Path("photos/embeddings")/f'{choice}_image_embeds.pt'}")
